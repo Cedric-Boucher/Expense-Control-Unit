45a7857d908abed1a0fb528b446b74f74b1f437a
Cedric Boucher <onebitalpha@gmail.com>
2025-07-21 22:17:05 -0600

    clean up frontend

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,9 @@
+import type { Transaction } from "./types";
+
 const API_BASE = 'http://localhost:8000';
 
-export async function getTransactions() {
-    const res = await fetch('${API_BASE}/transactions');
+export async function getTransactions(): Promise<Transaction[]> {
+    const res = await fetch(`${API_BASE}/api/transactions`);
+    if (!res.ok) throw new Error('Failed to fetch transactions');
     return await res.json();
 }
--- frontend/src/routes/transactions/+page.svelte
+++ frontend/src/routes/transactions/+page.svelte
@@ -1,21 +1,23 @@
-<script>
-  import { onMount } from 'svelte';
-  let transactions = [];
+<script lang="ts">
+	import { getTransactions } from '$lib/api';
+	import type { Transaction } from '$lib/types';
+    import { onMount } from 'svelte';
 
-  onMount(async () => {
-    const res = await fetch('http://localhost:8000/api/transactions');
-    transactions = await res.json();
-  });
+    let transactions: Transaction[] = [];
+
+    onMount(async () => {
+        transactions = await getTransactions()
+    });
 </script>
 
 <h1>Transactions</h1>
 
 {#if transactions.length}
-  <ul>
-    {#each transactions as tx}
-      <li>{tx.description} - ${tx.amount}</li>
-    {/each}
-  </ul>
+    <ul>
+        {#each transactions as tx}
+            <li>{tx.description} - ${tx.amount}</li>
+        {/each}
+    </ul>
 {:else}
-  <p>No transactions found.</p>
+    <p>No transactions found.</p>
 {/if}
