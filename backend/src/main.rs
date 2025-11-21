ce70268dc77761ce44ac28ab9a16580c701a202f
Cedric Boucher <onebitalpha@gmail.com>
2025-11-21 12:02:18 -0700

    enable network compression on server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,6 +3,7 @@ use std::{env, net::SocketAddr};
 use axum::{Extension, Router};
 use sqlx::migrate::Migrator;
 use tower_cookies::CookieManagerLayer;
+use tower_http::compression::{CompressionLayer, CompressionLevel, predicate::{Predicate, DefaultPredicate, SizeAbove}};
 use dotenv::dotenv;
 
 mod routes;
@@ -21,6 +22,13 @@ static MIGRATOR: Migrator = sqlx::migrate!();
 async fn main() {
     dotenv().ok();
 
+    const MIN_COMPRESSION_SIZE: u16 = 1000; // bytes
+    let compression_predicate = Predicate::and(
+        DefaultPredicate::new(),
+        SizeAbove::new(MIN_COMPRESSION_SIZE)
+    );
+    let compression_level = CompressionLevel::Default;
+
     let db = init_db_pool().await;
     MIGRATOR.run(&db).await.expect("Failed to run migrations");
 
@@ -36,7 +44,12 @@ async fn main() {
     let app = Router::new()
         .merge(api_routes)
         .layer(Extension(db))
-        .layer(CookieManagerLayer::new());
+        .layer(CookieManagerLayer::new())
+        .layer(
+            CompressionLayer::new()
+                .quality(compression_level)
+                .compress_when(compression_predicate)
+        );
 
     let port = env::var("PORT").unwrap_or_else(|_| "8000".to_string());
     let addr: SocketAddr = format!("0.0.0.0:{}", port).parse().unwrap();
