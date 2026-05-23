84cf14e4b24a03dfa43c316caa83e8b88e6b1366
Cedric Boucher <cedricjboucher@proton.me>
2026-05-23 16:12:14 -0600

    fix redirect issues

---
+++ frontend/src/routes/[...catchall]/+page.ts
@@ -0,0 +1,16 @@
+import { redirect } from '@sveltejs/kit';
+import { load_user } from '$lib/api';
+
+export async function load({ url }) {
+	// ignore background asset requests
+	if (url.pathname.includes('.')) {
+		return;
+	}
+	console.log("Catch-all route triggered for:", url.pathname);
+	const { user } = await load_user();
+	if (user) {
+		throw redirect(302, '/transactions');
+	} else {
+		throw redirect(302, '/login');
+	}
+}
