3cbc52ac68565648c0df59e8665139bb75edb0be
Cedric Boucher <cedricjboucher@proton.me>
2026-06-18 19:23:44 -0600

    add ability to filter transactions list by tags

--- frontend/src/routes/transactions/+page.ts
+++ frontend/src/routes/transactions/+page.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit';
-import { getTransactions, getCategories, load_user } from '$lib/api';
+import { getTransactions, getCategories, getTags, load_user } from '$lib/api';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async () => {
@@ -9,7 +9,11 @@ export const load: PageLoad = async () => {
 		throw redirect(302, '/login');
 	}
 
-	const [transactions, categories] = await Promise.all([getTransactions(), getCategories()]);
+	const [transactions, categories, tags] = await Promise.all([
+		getTransactions(),
+		getCategories(),
+		getTags()
+	]);
 
-	return { transactions, categories };
+	return { transactions, categories, tags };
 };
